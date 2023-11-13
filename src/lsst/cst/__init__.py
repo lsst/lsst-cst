a201bfdcccdc8e2d16e67bbcf63bfa53c09e0ae2
leannep <leanne@lsst.org>
2023-11-13 15:39:33 -0800

    Address K-T's comments

--- src/lsst/cst/__init__.py
+++ src/lsst/cst/__init__.py
@@ -1,1 +1,16 @@
 """Rubin LSST Community Science Tools"""
+
+__all__ = ["__version__"]
+
+from importlib.metadata import PackageNotFoundError, version
+
+__version__: str
+"""The version string of lsst
+(PEP 440 / SemVer compatible).
+"""
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
