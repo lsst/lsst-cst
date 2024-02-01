9c4bb4fd7ae5508c188d348b23e671894ec444ad
Cristobal Pio Garcia <cpio@sdfrome001.sdf.slac.stanford.edu>
2024-02-01 05:21:58 -0800

    add show scatter in DataImageDisplay

--- src/lsst/cst/visualization/data/displays.py
+++ src/lsst/cst/visualization/data/displays.py
@@ -1,6 +1,12 @@
 import holoviews as hv
 
 from lsst.cst.visualization.utils import ExposureData
+from collections.abc import Sequence
+from typing import Optional
+
+
+class ScatterOptions:
+    pass
 
 
 class Scatter:
@@ -8,11 +14,18 @@ class Scatter:
     def __init__(self, data: ExposureData):
         self._exposure_data = data
 
-    def show_scatter(self, data_x: str, data_y: str, frac: float = 1.0):
+    def show_scatter(self,
+                     columns: Optional[Sequence] = None,
+                     frac: float = 1.0,
+                     options: ScatterOptions = ScatterOptions()):
         data = self._exposure_data.get_data(frac)
+        if columns is None:
+            return hv.Scatter(data).options(toolbar=None)
         index = self._exposure_data.index
+        data_x = columns[0]
+        data_y = columns[1]
         assert data_x in index, f"Selected data {data_x} for X "\
                                 f"not available on exposure data"
-        assert data_x in index, f"Selected data {data_y} for Y "\
+        assert data_y in index, f"Selected data {data_y} for Y "\
                                 f"not available on exposure data"
         return hv.Scatter(data, data_x, data_y).options(toolbar=None)
