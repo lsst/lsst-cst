3796c705ad7a90ebe3f1e4ab02c4b61b55aa5ad9
Cristobal Pio Garcia <cpio@sdfrome002.sdf.slac.stanford.edu>
2024-02-09 13:05:00 -0800

    renaming files

---
+++ src/lsst/cst/visualization/params.py
@@ -0,0 +1,14 @@
+# Default parameters used in the Bokeh plot
+
+__all__ = ["PlotOptionsDefault"]
+
+
+class PlotOptionsDefault:
+    cmap_color = 'Viridis'
+    height = 600
+    marker = 'circle'
+    color = 'darkorange'
+    marker_color = 'darkorange'
+    marker_size = 5
+    toolbar_position = 'above'
+    width = 700
---
+++ src/lsst/cst/visualization/tools/deleters.py
@@ -0,0 +1,37 @@
+"""General plot utils"""
+
+import gc
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+
+# image deleters
+
+def delete_plot(plot: Figure) -> None:
+    """"""
+    pass
+
+
+def _remove_figure(fig: Figure) -> None:
+    """Remove a figure to reduce memory footprint.
+
+    Parameters
+    ----------
+    fig : `matplotlib.figure.Figure`
+        Figure to be removed.
+
+    Returns
+    -------
+    None
+    """
+    # Get the axes and clear their images
+    for ax in fig.get_axes():
+        for im in ax.get_images():
+            im.remove()
+    # Clear the figure
+    fig.clf()
+    # Close the figure
+    plt.close(fig)
+    # Call the garbage collector
+    gc.collect()
