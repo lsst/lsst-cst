401b9b27b6e24be61093c00d71c8b924d4119d99
Cristobal Pio Garcia <cpio@sdfrome001.sdf.slac.stanford.edu>
2024-03-01 02:01:39 -0800

    pre-commit changes

--- src/lsst/cst/conversions/__init__.py
+++ src/lsst/cst/conversions/__init__.py
@@ -4,11 +4,11 @@ data conversions and format
 
 from .tools import (
     data_id_to_str,
+    get_psf_properties,
     ids_to_str,
     shuffle_dataframe,
     sort_dataframe,
     tract_patch_from_ra_dec,
-    get_psf_properties
 )
 
 __all__ = [
@@ -18,5 +18,5 @@ __all__ = [
     "sort_dataframe",
     "TractPatchInformation",
     "tract_patch_from_ra_dec",
-    "get_psf_properties"
+    "get_psf_properties",
 ]
