22bdd9f68e3d09011dbe89a59b1045f763b8ccf5
Cristobal Pio Garcia <cpio@sdfrome001.sdf.slac.stanford.edu>
2024-03-01 01:38:23 -0800

    Solve minor problems exporting functions

--- src/lsst/cst/conversions/__init__.py
+++ src/lsst/cst/conversions/__init__.py
@@ -3,12 +3,12 @@ data conversions and format
 """
 
 from .tools import (
-    TractPatchInformation,
     data_id_to_str,
     ids_to_str,
     shuffle_dataframe,
     sort_dataframe,
     tract_patch_from_ra_dec,
+    get_psf_properties
 )
 
 __all__ = [
@@ -18,4 +18,5 @@ __all__ = [
     "sort_dataframe",
     "TractPatchInformation",
     "tract_patch_from_ra_dec",
+    "get_psf_properties"
 ]
